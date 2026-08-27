f3929fc2edd85e11d1f7f60d6c747b77b7bcc5db
snehagupta250904 <guptashena82@gmail.com>
2026-08-27 11:17:24 +0530

    Add solution for Lexicographically Smallest Permutation Greater Than Target

---
+++ LeetCode/Lexicographically-Smallest-Permutation-Greater-Than-Target/Solution.java
@@ -0,0 +1,9 @@
+bool solve(int i, bool flag) {
+    if (flag) {
+        // Still matching target exactly
+        // Need ch >= target[i] to stay valid
+    } else {
+        // Already greater than target
+        // Can use any character freely (fillRem)
+    }
+}
