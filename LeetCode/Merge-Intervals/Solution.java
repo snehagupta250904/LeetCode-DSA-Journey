6f78c0568c64cf50c51a7e4e8c67e4f1b31d943f
snehagupta250904 <guptashena82@gmail.com>
2026-09-03 23:56:49 +0530

    Add solution for Merge Intervals

---
+++ LeetCode/Merge-Intervals/Solution.java
@@ -0,0 +1,2 @@
+left_res = [ (intervals that do not overlap), (intervals that overlap with current node) ]
+right_res = [ (intervals that overlap with current node), (intervals that do not overlap) ]
